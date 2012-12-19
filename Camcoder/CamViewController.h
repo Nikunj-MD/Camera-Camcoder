6aee3ae674a2035ea4583ac735513129c1a2a3eb
Nikunj Modi <nikunjmodi@ADCs-Mac-mini-2.local>
2012-12-19 12:24:18 +0530

    Initial Commit

---
+++ Camcoder/CamViewController.h
@@ -0,0 +1,13 @@
+//
+//  CamViewController.h
+//  Camcoder
+//
+//  Created by Nikunj Modi on 19/12/12.
+//  Copyright (c) 2012 Nikunj Modi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CamViewController : UIViewController
+
+@end
---
+++ Camcoder/CamViewController.m
@@ -0,0 +1,29 @@
+//
+//  CamViewController.m
+//  Camcoder
+//
+//  Created by Nikunj Modi on 19/12/12.
+//  Copyright (c) 2012 Nikunj Modi. All rights reserved.
+//
+
+#import "CamViewController.h"
+
+@interface CamViewController ()
+
+@end
+
+@implementation CamViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
